e993496201e2beaa237810808f0f66a808558422
Silvycr <silvia_alinacr@live.com>
2023-09-06 15:50:08 -0700

    Adding the db

--- models/city.py
+++ models/city.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from os import getenv
 from models.base_model import BaseModel, Base
-import models
+from models.state import State
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+import os 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
+        __tablename__ = "cities"
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
-    else:
-        name = ''
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship(
+                                "Place",
+                                backref="cities",
+                                cascade="all, delete-orphan"
+                             )
+else:
+    class City(BaseModel):
         state_id = ''
+        name = ''
