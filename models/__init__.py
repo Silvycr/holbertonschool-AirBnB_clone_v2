76d6dd378ddaaa4296b02b731e981c59e0a839eb
Silvycr <silvia_alinacr@live.com>
2023-09-06 13:10:09 -0700

    Adding parameters for the database connection

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from os import getenv
-from models.base_model import BaseModel
-import models
-import storage
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+import os
 
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    else:
-        name = ''
-        state_id = ''
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
