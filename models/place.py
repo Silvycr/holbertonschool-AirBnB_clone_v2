7ece8f44cc0bc71976b48db4cf154b70fce74631
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-09-06 16:32:04 -0500

    update places to create table correctly

--- models/place.py
+++ models/place.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models import storage
-from models.place import Place
-from models.city import City
-from models.user import User
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
-city_id = '<existing city id>'
-user_id = '<existing user id>'
-
-place_attrs = {
-    'city_id': city_id,
-    'user_id': user_id,
-    'name': 'House',
-    'description': 'des',
-    'number_rooms': 4,
-    'number_bathrooms': 2,
-    'max_guest': 6,
-    'price_by_night': 100,
-    'latitude': 1.3,
-    'longitude': 2.3
-}
-
-new_place = Place(**place_attrs)
-
-storage.new(new_place)
-storage.save()
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey("city_id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("user_id"), nullable=False)
+    name = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
