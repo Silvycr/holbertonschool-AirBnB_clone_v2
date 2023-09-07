c3e0b689cb721239dea3af9f6fff2cd6d047891b
Denzel18rb <74129875rb@gmail.com>
2023-09-06 21:55:38 -0700

    upgrade two models for full operation

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place",
+            secondary="place_amenity",
+            viewonly=False,
+            back_populates="amenities"
+        )
+    
+    else:
+        name = ""
