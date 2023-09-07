027ab3c13b3121161c4603107b6bc7ffa1e7c15a
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-09-06 23:59:20 -0500

    modify the amenity model

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-import os
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
-HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
-class Amenity(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
-    if HBNB_TYPE_STORAGE == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place",
-            secondary="place_amenity",
-            viewonly=False,
-            back_populates="amenities"
-        )
-    
-    else:
-        name = ""
+class Amenity(BaseModel, Base):
+    """
+    Amenity inherits from BaseModel and Base (respect the order)
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    places_amenities = relationship(
+        "Place",
+        secondary="place_amenity")
