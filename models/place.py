a7bcafe7421a00114d473e8133fa383f9dce7c7c
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-09-08 11:10:27 -0500

    Update place.py for pycodestyle

--- models/place.py
+++ models/place.py
@@ -5,18 +5,16 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
-
 if getenv("HBNB_TYPE_STORAGE") == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                        Column('place_id', String(60),
-                                ForeignKey('places.id'),
-                                primary_key=True, nullable=False),
-                        Column('amenity_id', String(60),
-                                ForeignKey('amenities.id'),
-                                primary_key=True, nullable=False))
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
     class Place(BaseModel, Base):
-
         __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -29,10 +27,10 @@ if getenv("HBNB_TYPE_STORAGE") == 'db':
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref='place',
-                                cascade='all, delete, delete-orphan')
+                              cascade='all, delete, delete-orphan')
         amenities = relationship("Amenity", secondary=place_amenity,
-                                  back_populates="place_amenities",
-                                  viewonly=False)
+                                back_populates="place_amenities",
+                                viewonly=False)
 else:
     class Place(BaseModel):
         """ A place to stay """
